17734e9c2c5c1e56a7b5cce0e70794e03791e285
cuong292 <nghiembacuong1234@gmail.com>
2019-05-17 13:55:41 +0700

    config

--- src/main/java/com/demo/springtest/config/RestMVCConfiguration.java
+++ src/main/java/com/demo/springtest/config/RestMVCConfiguration.java
@@ -1,12 +1,36 @@
 package com.demo.springtest.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan("com.demo.springtest")
+@PropertySource({"classpath:mysql-props.properties"})
 public class RestMVCConfiguration {
 
+    @Autowired
+    Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (Exception e){
+            System.out.println("driver not found");
+        }
+        dataSource.setUser(env.getProperty("jdbc.username"));
+        dataSource.setPassword(env.getProperty("jdbc.password"));
+        dataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        return dataSource;
+    }
 }
---
+++ src/main/java/com/demo/springtest/config/dao/TestDAOImpl.java
@@ -0,0 +1,10 @@
+package com.demo.springtest.config.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TestDAOImpl implements TestDAO{
+    @Autowired
+    
+}
