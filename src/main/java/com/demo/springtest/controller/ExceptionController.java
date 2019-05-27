64253bd78c2f8eda3431506db8685c0dd2afb355
cuong292 <nghiembacuong1234@gmail.com>
2019-05-27 18:21:46 +0700

    first commit

--- src/main/java/com/demo/springtest/controller/ExceptionController.java
+++ src/main/java/com/demo/springtest/controller/ExceptionController.java
@@ -3,6 +3,7 @@ package com.demo.springtest.controller;
 import com.demo.springtest.response.BaseError;
 import com.demo.springtest.response.BaseResponse;
 import com.demo.springtest.response.BaseResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,6 +17,17 @@ public class ExceptionController {
         BaseError error = new BaseError();
         error.setMessage(e.getMessage());
         error.setTimeStamp(Calendar.getInstance().getTimeInMillis());
-        error.setStatus(HttpRes);
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new BaseResponseEntity(baseResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public BaseResponseEntity handleException(NumberFormatException e) {
+        BaseResponse baseResponse = new BaseResponse();
+        BaseError error = new BaseError();
+        error.setMessage("Number format wrong");
+        error.setTimeStamp(Calendar.getInstance().getTimeInMillis());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return new BaseResponseEntity(baseResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/demo/springtest/response/BaseResponseEntity.java
+++ src/main/java/com/demo/springtest/response/BaseResponseEntity.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class BaseResponseEntity extends ResponseEntity<BaseResponse> {
+
     public BaseResponseEntity(BaseResponse body, HttpStatus status) {
         super(body, status);
     }
